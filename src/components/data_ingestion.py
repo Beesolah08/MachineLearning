f8ff77fb6f2772e16c48b4409a1bed119ae06c56
Beesolah08 <beesolah59@gmail.com>
2025-09-20 11:58:57 +0100

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 @dataclass
 class dataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', "train.csv")
@@ -51,6 +55,9 @@ class dataIngestion:
 
 if __name__ == "__main__":
     obj = dataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
+
     # FIX 2: Ensure logging messages are written to the file before the program exits.
     logging.shutdown()
